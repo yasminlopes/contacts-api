fcd84491889cfda9d8ab067686b7760664bf3cc7
Yasmin Lopes <yclopes27@gmail.com>
2025-04-25 01:46:43 -0300

    feat: add contact update validation schema using Joi

---
+++ src/validators/contact.validator.ts
@@ -0,0 +1,10 @@
+import Joi from 'joi'
+import { joiFieldMessages } from '../utils/joi-field-messages'
+import { REGEX } from '../utils/regex'
+
+export const contactUpdateSchema = Joi.object({
+  name: Joi.string().min(3).max(100).required().messages(joiFieldMessages('nome', { min: 3, max: 100 })),
+  cpf: Joi.string().length(11).pattern(REGEX.ONLY_NUMBERS).required().messages({ ...joiFieldMessages('CPF', { length: 11 }), 'string.pattern.base': 'O CPF deve conter apenas números.' }),
+  email: Joi.string().email().required().messages({ ...joiFieldMessages('e-mail'), 'string.email': 'O e-mail deve ser válido.' }),
+  phone: Joi.string().min(10).max(15).required().messages(joiFieldMessages('telefone', { min: 10, max: 15 }))
+})
