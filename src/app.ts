7e1d48a7bfd7335cda3d6099990bfee20dd88992
Yasmin Lopes <yclopes27@gmail.com>
2025-04-27 11:54:54 -0300

    fix: cors issue

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,6 @@ import cors from '@fastify/cors'
 import { appRoutes } from './http/routes'
 
 export async function appPlugin (app: FastifyInstance): Promise<void> {
-  await app.register(cors, {
-    origin: '*'
-  })
+  await app.register(cors, { origin: true, methods: ['GET', 'POST', 'PATCH', 'DELETE'] })
   await app.register(appRoutes)
 }
